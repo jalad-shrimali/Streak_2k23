09262ca8370ea0a9e9217cc7cf0366b19e92afdb
Jalad Shrimali <78215000+jalad-shrimali@users.noreply.github.com>
2023-02-21 08:19:21 +0530

    Create leetcode 540.java

---
+++ leetcode 540.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (mi % 2 == 1) mi--;
+            
+            if (nums[mi] != nums[mi + 1]) {
+                // We didn't find a pair. The single element must be on the left.
+                // (pipes mean start & end)
+                // Example: |0 1 1 3 3 6 6|
+                //               ^ ^
+                // Next:    |0 1 1|3 3 6 6
+                hi = mi;
+            } else {
+                // We found a pair. The single element must be on the right.
+                // Example: |1 1 3 3 5 6 6|
+                //               ^ ^
+                // Next:     1 1 3 3|5 6 6|
+                lo = mi + 2;
+            }
+        }
+        return nums[lo];
+    }
+}
