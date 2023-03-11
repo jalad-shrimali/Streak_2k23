b29d408d7df49dd894bf879c1193893cdd69c4f4
Jalad Shrimali <78215000+jalad-shrimali@users.noreply.github.com>
2023-03-11 11:30:02 +0530

    Create leetcode 109.java

---
+++ leetcode 109.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null) return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        ListNode pre = null;
+        while(fast != null && fast.next != null){
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null;
+        TreeNode ret = new TreeNode(slow.val);
+        ret.left = sortedListToBST(dummy.next);
+        ret.right = sortedListToBST(slow.next);
+        return ret;
+    }
+}
