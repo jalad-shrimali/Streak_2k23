6238e967549a8e711895320e8e561f771054c8b8
Jalad Shrimali <78215000+jalad-shrimali@users.noreply.github.com>
2023-04-11 08:36:56 +0530

    Create leetcode 2390.java

---
+++ leetcode 2390.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> stack= new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='*'){
+                stack.push(s.charAt(i));
+                continue;
+            }
+            else{
+                stack.pop();
+            }
+        }
+        if(stack.isEmpty()) return "";
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
